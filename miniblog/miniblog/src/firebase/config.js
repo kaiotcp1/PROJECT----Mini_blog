999eb7d14d8245801dc01698967b6b3818d46873
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-01-31 17:42:58 -0300

    Configurando Firebase ao React

---
+++ miniblog/miniblog/src/firebase/config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAL5BzFQ_sPfEA2UH1hnWsn8sb-2Dh-KlM",
+  authDomain: "miniblog-380fd.firebaseapp.com",
+  projectId: "miniblog-380fd",
+  storageBucket: "miniblog-380fd.appspot.com",
+  messagingSenderId: "653634288221",
+  appId: "1:653634288221:web:d669b490250d07bc66abb4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFireStore(app);
+
+export { db };
