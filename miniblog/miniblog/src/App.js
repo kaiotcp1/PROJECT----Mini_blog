bbce1e45a5406e8c13f4beddefb206e78ba3f12b
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-01-31 18:26:53 -0300

    Adicionando componente de navbar

--- miniblog/miniblog/src/App.js
+++ miniblog/miniblog/src/App.js
@@ -1,23 +1,28 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-
+// Components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 // Pages
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 
+
 function App() {
   return (
-    <div className="App"> 
-    <BrowserRouter>
-      <div className="container">
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
