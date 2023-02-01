92ee1242a6e685645a68ef7403a057471f12ff61
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-01-31 21:15:14 -0300

    State do form de cadastro e atualização CSS global

--- miniblog/miniblog/src/pages/register/Register.js
+++ miniblog/miniblog/src/pages/register/Register.js
@@ -3,11 +3,38 @@ import styles from './Register.module.css';
 import { useState, useEffect } from 'react';
 
 function Register() {
+    const [displayName, setDisplayName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setError("");
+
+        const user = {
+            displayName,
+            email,
+            password
+        }
+
+        // Validação, senha do usuário
+        if(password !== confirmPassword) {
+            setError("As senhas precisam ser iguais !")
+            return
+        }
+
+        console.log(user);
+    }
+
+    
+    
     return (
-        <div>
+        <div className={styles.register}>
             <h1>Cadastre-se para postar</h1>
             <p>Crie seu usuário e compartilhe susa histórias</p>
-            <form >
+            <form onSubmit={handleSubmit}>
                 <label >
                     <span>Nome:</span>
                     <input
@@ -15,6 +42,8 @@ function Register() {
                         name='displayName'
                         required
                         placeholder='Nome do usuário'
+                        value={displayName}
+                        onChange={(e) => setDisplayName(e.target.value)}
                     />
                 </label>
                 <label >
@@ -23,6 +52,8 @@ function Register() {
                         name='email'
                         required
                         placeholder='E-mail do usuário'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                 </label>
                 <label >
@@ -31,6 +62,8 @@ function Register() {
                         name='password'
                         required
                         placeholder='Insira sua senha'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                 </label>
                 <label >
@@ -39,9 +72,12 @@ function Register() {
                         name='confirmPassword'
                         required
                         placeholder='Confirme a sua senha'
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
                     />
                 </label>
                 <button className='btn'>Cadastrar</button>
+                {error && <p className='error'>{error}</p>}
             </form>
         </div>
     )
